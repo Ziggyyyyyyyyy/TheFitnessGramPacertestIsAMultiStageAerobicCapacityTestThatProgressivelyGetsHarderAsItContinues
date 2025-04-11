6fabb001ec8bec3565c69efc442e56ae5532dfec
Ziggyyyyyyyyy <27zane.miller@boiseschools.net>
2025-04-11 17:11:11 +0000

    dsbsd

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public class hello {
     public void greet() {
         System.out.println("Hello, Ziggy from an object!");
     }
+    
     public static void main (String[] args) {
        hello hello = new hello();
        hello.greet();
