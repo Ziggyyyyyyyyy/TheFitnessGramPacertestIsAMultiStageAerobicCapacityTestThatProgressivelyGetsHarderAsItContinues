93084a1fb4bbd870574fa94f946f59f54fdb6dfd
Ziggyyyyyyyyy <27zane.miller@boiseschools.net>
2025-04-11 17:22:41 +0000

    adwadwa

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
 
     public void greet() {
-        System.out.println("Hello, Ziggy from an object!");
+        System.out.println("Hello, " +name+ "!");
     }
     public String getNameFromUser() {
     System.out.print("Please enter your name: ");
