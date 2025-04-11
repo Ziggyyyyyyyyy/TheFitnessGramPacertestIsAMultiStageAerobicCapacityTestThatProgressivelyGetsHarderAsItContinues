08ee126e62cc1c34ff7ff5fc69ae6f2f8510e40f
Ziggyyyyyyyyy <27zane.miller@boiseschools.net>
2025-04-11 17:21:12 +0000

    87tggui

--- hello.java
+++ hello.java
@@ -3,9 +3,17 @@ public class hello {
     public void greet() {
         System.out.println("Hello, Ziggy from an object!");
     }
-    
+    public String getNameFromUser() {
+    System.out.print("Please enter your name: ");
+    java.util.Scanner scanner = new java.util.Scanner(System.in);
+    String name = scanner.nextLine();
+    scanner.close();
+    return name;
+}
     public static void main (String[] args) {
        hello hello = new hello();
+       Dtring name = hello.getNameFromUser();
        hello.greet();
     }
 }
+
